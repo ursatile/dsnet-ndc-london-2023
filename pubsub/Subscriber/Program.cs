af5efaef23a9f8a91deea72b5ce39db900446103
Dylan Beattoie <dylan@dylanbeattie.net>
2023-01-23 15:49:40 +0000

    Working pubsub demo

--- pubsub/Subscriber/Program.cs
+++ pubsub/Subscriber/Program.cs
@@ -4,7 +4,10 @@ using Messages;
 const string AMQP = "amqps://spjuzoxw:mMT6Y4tSsLW6I6jX3FTIcjRDUodd7JaD@feisty-maroon-kiwi.rmq5.cloudamqp.com/spjuzoxw";
 
 var bus = RabbitHutch.CreateBus(AMQP);
-bus.PubSub.Subscribe<Greeting>("SUBSCRIPTION_ID", greeting => {
+bus.PubSub.Subscribe<Greeting>("dylanbeattie", greeting => {
+    if (greeting.Number % 5 == 0) {
+        throw new Exception("Hey! I don't like 5!");
+    }
     Console.WriteLine(greeting);
 });
 
