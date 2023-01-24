e2c8ad200ac46458e89934e60f5598b47e113b79
Dylan Beattoie <dylan@dylanbeattie.net>
2023-01-24 11:29:08 +0000

    gRPC greeting demo

---
+++ grpc/GrpcClient/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+using GrpcServer;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7165");
+var grpcClient = new Greeter.GreeterClient(channel);
+Console.WriteLine("Connected to gRPC! press a key to send a message...");
+while(true) {
+    Console.ReadKey(true);
+    var request = new HelloRequest {
+        Name = "NDC London"
+    };
+    var reply = grpcClient.SayHello(request);
+    Console.WriteLine(reply.Message);
+}
+
+			
