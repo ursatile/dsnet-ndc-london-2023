1214a67994eb0080419d15f89ac0b6f39bdd889c
Dylan Beattoie <dylan@dylanbeattie.net>
2023-01-24 11:37:25 +0000

    gRPC working with real certificate on port 5003

--- grpc/GrpcClient/Program.cs
+++ grpc/GrpcClient/Program.cs
@@ -1,18 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 
 using Grpc.Net.Client;
 using GrpcServer;
 
-using var channel = GrpcChannel.ForAddress("https://localhost:7165");
+using var channel = GrpcChannel.ForAddress("https://workshop.ursatile.com:5003");
 var grpcClient = new Greeter.GreeterClient(channel);
 Console.WriteLine("Connected to gRPC! press a key to send a message...");
-while(true) {
-    Console.ReadKey(true);
-    var request = new HelloRequest {
-        Name = "NDC London"
-    };
-    var reply = grpcClient.SayHello(request);
-    Console.WriteLine(reply.Message);
+while (true) {
+	Console.ReadKey(true);
+	var request = new HelloRequest {
+		Name = "NDC London"
+	};
+	var reply = grpcClient.SayHello(request);
+	Console.WriteLine(reply.Message);
 }
 
-			
--- grpc/GrpcServer/Program.cs
+++ grpc/GrpcServer/Program.cs
@@ -1,5 +1,10 @@
+using System.Net;
 using GrpcServer.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(WithUrsatileCertificate);
 // Add services to the container.
 builder.Services.AddGrpc();
 
@@ -10,3 +15,16 @@ app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
+
+void WithUrsatileCertificate(KestrelServerOptions options) {
+	var pfxPassword = Environment.GetEnvironmentVariable("UrsatilePfxPassword") ?? "";
+	var https = UseCertIfAvailable(@"D:\Dropbox\workshop.ursatile.com\workshop.ursatile.com.pfx", pfxPassword);
+	options.ListenAnyIP(5002, listenOptions => listenOptions.Protocols = HttpProtocols.Http1AndHttp2);
+	options.Listen(IPAddress.Any, 5003, https);
+	options.AllowSynchronousIO = true;
+}
+
+Action<ListenOptions> UseCertIfAvailable(string pfxFilePath, string pfxPassword) {
+	if (File.Exists(pfxFilePath)) return listen => listen.UseHttps(pfxFilePath, pfxPassword);
+	return listen => listen.UseHttps();
+}
