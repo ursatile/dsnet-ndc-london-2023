cf08a837850388cca736b7e55ae4a5e14b968302
Dylan Beattoie <dylan@dylanbeattie.net>
2023-01-24 10:08:34 +0000

    GraphQL works!

--- dotnet/Autobarn.Website/GraphQL/Queries/VehicleQuery.cs
+++ dotnet/Autobarn.Website/GraphQL/Queries/VehicleQuery.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Autobarn.Data;
 using Autobarn.Data.Entities;
 using Autobarn.Website.GraphQL.GraphTypes;
@@ -14,6 +16,30 @@ namespace Autobarn.Website.GraphQL.Queries {
 			Field<ListGraphType<VehicleGraphType>>("Vehicles")
 				.Description("Return all vehicles")
 				.Resolve(GetAllVehicles);
+			Field<VehicleGraphType>("Vehicle")
+				.Description("Return a single vehicle")
+				.Arguments(MakeNonNullStringArgument("registration", "The registration of the vehicle you are querying"))
+				.Resolve(GetVehicle);
+			Field<ListGraphType<VehicleGraphType>>("VehiclesByColor")
+				.Description("Return vehicles matching a specified color")
+				.Arguments(MakeNonNullStringArgument("color", "The color of cars you want"))
+				.Resolve(GetVehiclesByColor);
+		}
+
+		private IEnumerable<Vehicle> GetVehiclesByColor(IResolveFieldContext<object> context) {
+			var color = context.GetArgument<string>("color");
+			return db.ListVehicles().Where(v => v.Color.Contains(color, StringComparison.InvariantCultureIgnoreCase));
+		}
+
+		private Vehicle GetVehicle(IResolveFieldContext<object> context) {
+			var registration = context.GetArgument<string>("registration");
+			return db.FindVehicle(registration);
+		}
+
+		private QueryArgument MakeNonNullStringArgument(string name, string description) {
+			return new QueryArgument<NonNullGraphType<StringGraphType>> {
+				Name = name, Description = description
+			};
 		}
 
 		private IEnumerable<Vehicle> GetAllVehicles(IResolveFieldContext<object> arg)
